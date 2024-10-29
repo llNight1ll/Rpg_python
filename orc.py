ad68948ba1d4117f64b56799f68df4a0e4cbff43
llNightll <sami.hmida94@gmail.com>
2024-10-29 20:16:07 +0100

    animation attaque joueur

--- orc.py
+++ orc.py
@@ -77,7 +77,41 @@ text = """
                                                    %=*++==*%                                               
                                                    +++*==*+#                                               
                                                   %----=*+%                                                
-                                                    #=% %*%                                                 
+                                                    #=% %*%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                  
+                                                                                                 
 """
 player = """                                                                                                                                                                                                     
                                                              =:::=                                  
@@ -230,42 +264,51 @@ def ennemy_attack_animation():
   player_attack_animation()
 
 
-
 def player_attack_animation():
-  clear_terminal()
 
   i = 1
-  indent_delet = " " * (250 - 50* (5-i))
-  indent = " " * (50* (5-i))
-
 
 
   while i < 5 :
     # Nombre d'espaces pour décaler le dessin
 
+    indent = " " * (50*i)
+    indent2 = " " * (250 - (50*i))
+
+
+
     # Ajouter des espaces avant chaque ligne de l'orc
-    orc_centered = "\n".join([indent_delet + line1 + indent + line2 for line1, line2 in zip(player.splitlines(), text.splitlines())])
+    orc_centered = "\n".join([indent + line1  + indent2 + line2 for line1, line2 in zip(player.splitlines(), text.splitlines())])
+    print(orc_centered)
+    time.sleep(0.1)
+    clear_terminal()
+    i += 1
+
+  while i < 5 :
+    # Nombre d'espaces pour décaler le dessin
+    indent = " " * (50* (5-i))
+    indent2 = " " * (250 - (50* (5-i)))
+
+
+    # Ajouter des espaces avant chaque ligne de l'orc
+    orc_centered = "\n".join([ indent + line1  + line2 for line1, line2 in zip(player.splitlines(), text.splitlines())])
     print(orc_centered)
     time.sleep(0.1)
     clear_terminal()
     i += 1
   i = 1
-  while i < 5 :
-    # Nombre d'espaces pour décaler le dessin
-    # Ajouter des espaces avant chaque ligne de l'orc
-    orc_centered = "\n".join([indent_delet + line1 +  indent  + line2 for line1, line2 in zip(player.splitlines(), text.splitlines())])
-    print(orc_centered)
-    time.sleep(0.1)
-    clear_terminal()
-    i += 1
+ 
       
-  i = 0
+  indent = " " * 250
 
   # Ajouter des espaces avant chaque ligne de l'orc
-  orc_centered = "\n".join([line1 +  indent + line2 for line1, line2 in zip(player.splitlines(), text.splitlines())])
+  orc_centered = "\n".join([line1 + indent + line2 for line1, line2 in zip(player.splitlines(), text.splitlines())])
   print(orc_centered)
 
   draw_hp.draw_hp_ennemy(40, "red", 100, 100)
+
+
+
 
 
 
